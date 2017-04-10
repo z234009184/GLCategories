156b2f5e330ef23fce5a19dac4b3ba0eb5516d5f
15004657729@163.com <15004657729@163.com>
2017-04-10 11:13:41 +0800

    fix

---
+++ GLCategories/GLCategories/GLCategories.h
@@ -0,0 +1,13 @@
+//
+//  GLCategories.h
+//  GLCategories
+//
+//  Created by 张国梁 on 2017/4/10.
+//  Copyright © 2017年 张国梁. All rights reserved.
+//
+
+#ifndef GLCategories_h
+#define GLCategories_h
+
+
+#endif /* GLCategories_h */
